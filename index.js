8c2b348572724a2c208d1b4accf89a982336b678
bikoil <bikokhaled13@gmail.com>
2024-05-07 21:31:55 +0300

    Edited the output of starting the bot

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`Magicord is online! Logged in as ${readyClient.user.tag}`);
 });
 
 // Log in to Discord with your client's token
