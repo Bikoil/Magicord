dffa14dfb9075ad9aacfea9cf1e16197493922db
bikoil <bikokhaled13@gmail.com>
2024-05-08 21:11:56 +0300

    Added echo command restrictions

--- commands/utility/echo.js
+++ commands/utility/echo.js
@@ -9,6 +9,12 @@ module.exports = {
                 .setDescription('The message you want the bot to say')
                 .setRequired(true)),
     async execute(interaction) {
+        // Check if the user has administrator permissions
+        if (!interaction.member.permissions.has('ADMINISTRATOR')) {
+            return await interaction.reply({ content: "You don't have permission to use this command `MISSING PERMISSIONS: ADMINISTRATOR`", ephemeral: true });
+        }
+
+        // User has administrator permissions, proceed with the command execution
         const message = interaction.options.getString('message');
         await interaction.reply(message);
     },
