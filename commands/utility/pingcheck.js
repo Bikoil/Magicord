6bb1e87d98926f081b1b4aced170d4e051735534
bikoil <bikokhaled13@gmail.com>
2024-05-11 15:10:33 +0300

    Changed pingcheck message

--- commands/utility/pingcheck.js
+++ commands/utility/pingcheck.js
@@ -7,6 +7,6 @@ module.exports = {
 		.setName('pingcheck')
 		.setDescription('Check if the bot is up and running!'),
 	async execute(interaction) {
-		await interaction.reply(`Pong! The bot is up and running using \`node index.js\` currently running on ${system}`);
+		await interaction.reply(`Server is setup! The bot is up and running using \`node index.js\` currently running on ${system}`);
 	},
 };
