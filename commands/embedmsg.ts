f0355ff7dd057bae1ca09fba3b9fe81bb4c3457e
bikoil <bikoil@proton.me>
2024-07-31 02:14:33 +0300

    Fixed and Ported embedmsg command

--- commands/embedmsg.ts
+++ commands/embedmsg.ts
@@ -11,7 +11,7 @@ export class MyDiscordBot {
         @SlashOption({
             name: 'title',
             description: 'The title of the embed',
-            type: ApplicationCommandOptionType.STRING,  // Corrected type
+            type: ApplicationCommandOptionType.String,  // Corrected type
             required: false,
         })
         title: string | null,
@@ -19,14 +19,14 @@ export class MyDiscordBot {
         @SlashOption({
             name: 'body',
             description: 'The body of the embed',
-            type: ApplicationCommandOptionType.STRING,  // Corrected type
+            type: ApplicationCommandOptionType.String,  // Corrected type
         })
         body: string,
 
         @SlashOption({
             name: 'footer',
             description: 'The footer of the embed',
-            type: ApplicationCommandOptionType.STRING,  // Corrected type
+            type: ApplicationCommandOptionType.String,  // Corrected type
             required: false,
         })
         footer: string | null,
